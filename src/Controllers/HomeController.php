689baca4f61bfd3858202b562783dab9c0a18cb6
Marvin Menchu <mmenchu@outlook.com>
2023-02-24 15:40:03 -0600

    Se agrego HomeController y las visatas layout  y home

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controllers;
+use App\Http\Response;
+class HomeController
+{
+  /** muestra una lista de este recurso */
+  public function index(){
+    return new Response('home');
+  }
+  /** muestra un formulario para crear un nuevo recurso*/
+  public function create(){}
+  /** gurda el recurso del metodo create() en la base de datos */
+  public function store(){}
+  /** Muestra un único recurso especificado*/
+  public function show(){}
+  /**  Muestra el formulario para editar un recurso*/
+  public function edit(){}
+  /**  Actualiza un recurso específico en la base de datos*/
+  public function update(){}
+  /** Elimina un recurso específico de la base de datos*/
+  public function Destroy(){}
+}
