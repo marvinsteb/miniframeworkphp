f25d480f9f3082fb574483a8ee88518d2d89a363
Marvin Menchu <mmenchu@outlook.com>
2023-02-24 15:30:43 -0600

    Se agrego la clase Response

---
+++ src/Http/Response.php
@@ -0,0 +1,20 @@
+<?Php
+namespace App\Http;
+class Response 
+{
+  protected $view; //array, json pdf..
+  public function __construct($view)
+  {
+    $this->view = $view;
+  }
+  public function getView()
+  {
+    return $this->view;
+  }
+  public function send()
+  {
+    $view = $this->view;
+    $routeView = file_get_contents(__DIR__ . "/../../views/{$view}.php");
+    require __DIR__ . "/../../views/layout.php";
+  }
+}
